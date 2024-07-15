6a4d1192c942758126710a953a1e3a4ba9ebaef6
Shailja Bhargava <140694127+Shailja004@users.noreply.github.com>
2024-07-15 18:30:55 +0530

    Add files via upload

---
+++ Collection Assignment 5/Program.cs
@@ -0,0 +1,35 @@
+﻿//Assignment 5: Combining Lists and Dictionaries.
+
+using System;
+namespace Assignment5
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<int>> StudentScores = new Dictionary<string, List<int>>();
+            
+              StudentScores.Add("Shailja", new List<int> { 95, 92, 98 } );
+              StudentScores.Add("Raunak", new List<int> { 78, 81, 79 });
+              StudentScores.Add("Vanshika", new List<int> { 90, 85, 87 });
+
+            Console.WriteLine("Student Scores:");
+            foreach (var student in StudentScores)
+            {
+                
+                Console.WriteLine(student.Key + "  " + string.Join("-", student.Value) );
+            }
+
+            Console.WriteLine("\nAverage Scores: ");
+            foreach (var student in StudentScores)
+            {
+                double averageScore = student.Value.Average();
+                Console.WriteLine(student.Key + "  " + averageScore);
+            }
+
+            var highestaveragescore = StudentScores.OrderByDescending(s => s.Value.Average()).First();
+            Console.WriteLine("\nTop Student:" + "  " + highestaveragescore.Key + "  " + highestaveragescore.Value.Average());
+        }
+    }
+}
+
