46736b3e041dd1a950313c3f0ef12e3f0b82547a
Shailja Bhargava <140694127+Shailja004@users.noreply.github.com>
2024-07-15 18:28:37 +0530

    Add files via upload

---
+++ Collection Assignment 3/Program.cs
@@ -0,0 +1,47 @@
+﻿//Assignment 3: Dictionary Basics
+
+using System;
+
+namespace Assignment3
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> student = new Dictionary<string, int>();
+
+
+            student.Add("Shailja",96);
+            student.Add("Raunak",92);
+            student.Add("Aryan",93);
+            student.Add("Vanshika",84);
+            student.Add("Deepa",75);
+            
+            Console.WriteLine("Students and Scores");
+            foreach (var stud in student)
+            {
+                Console.WriteLine(stud.Key + "  " +stud.Value);
+            }
+
+
+            student.Add("Safal",  66);
+            Console.WriteLine("\nUpdated dictionary:");
+            foreach (var stud in student)
+            {
+                Console.WriteLine(stud.Key + "  " + stud.Value);
+            }
+
+            student.Remove("Deepa");
+            Console.WriteLine("\nafter removing Deepa:");
+            foreach (var stud in student)
+            {
+                Console.WriteLine(stud.Key + "  " + stud.Value);
+            }
+
+        
+            int rollnum = student["Shailja"];
+            Console.WriteLine("\nShailja's Scores is " +"  " + rollnum);
+        }
+    }
+}
+
